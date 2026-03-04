c78874b15b548278c6a95ac9db84c95666535da1
tkrosmkn2sebulu-lang <tkrosmkn2sebulu@gmail.com>
2026-03-04 14:42:05 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -12,5 +12,3 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('fetch', (event) => {
   // Hanya Service Worker pass-through standar agar lolos syarat PWA
 });
-
-**Selesai!** Jika ketiga file (`index.html`, `manifest.json`, dan `sw.js`) sudah ada di dalam satu folder dan Anda unggah ke Vercel, maka saat dibuka melalui Chrome (di HP atau Laptop), akan muncul **tombol hijau "Install App"** di pojok kanan atas (serta di halaman depan saat Anda baru membuka aplikasi).
